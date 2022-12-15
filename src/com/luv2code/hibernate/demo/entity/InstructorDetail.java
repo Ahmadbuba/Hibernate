efceb5ae3d4396c871c9ff77290d3016a67788b4
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-12-15 18:15:17 +0100

    feat- looked at oneToone bi-directional where one change on one table affects the other because the cascade type is CascadeType.All

--- src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -33,6 +35,13 @@ public class InstructorDetail {
 	
 	@Column(name="hobby")
 	private String hobby;
+	
+	// add new field for Instructor (also add getter/setters)
+	
+	// add @OneToOne annotation
+	
+	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	private Instructor instructor;
 	
 	public InstructorDetail() {
 		
@@ -65,6 +74,15 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+	
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	@Override
