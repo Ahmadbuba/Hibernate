b781e2098b328396b1a9bb71e70e125478e31b2c
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-12-17 08:48:59 +0100

    looked one to one mapping but bi-directional which unlike uni we can access the instructor from instructor detail also and we also looked at how by omiting cascadetype.remove on the instructor field in the Instructor detail class alongside setting the Instructordetail null in the main class will only remove the instructorDetail and wont affect the instructor

--- src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
+++ src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -28,7 +28,7 @@ public class DeleteInstructorDetailDemo {
 			session.beginTransaction();
 			
 			// get the instructor detail object
-			int theId = 2;
+			int theId = 3;
 			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
 			
 			// print the instructor detail
@@ -40,6 +40,12 @@ public class DeleteInstructorDetailDemo {
 			// now let's delete the instructor detail
 			
 			System.out.println("Deleting tempInstructorDetail: " + tempInstructorDetail);
+			
+			// remove the associated object reference
+			// break bi-directional link
+			
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			
 			session.delete(tempInstructorDetail);
 			
 			// commit transaction
--- src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -40,7 +40,7 @@ public class InstructorDetail {
 	
 	// add @OneToOne annotation
 	
-	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	@OneToOne(mappedBy="instructorDetail", cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
 	private Instructor instructor;
 	
 	public InstructorDetail() {
