89670695df78a622d827481c5e60026c0a00c86d
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-12-17 17:40:57 +0100

    project workspace setup to begin work

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -44,6 +44,8 @@ public class Course {
 	@JoinColumn(name="course_id")
 	private List<Review> reviews;
 	
+	private List<Student> students;
+	
 	public Course() {
 		
 	}
@@ -94,7 +96,24 @@ public class Course {
 		reviews.add(theReview);
 	}
 	
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
 	
+	// add a convenience method
+	
+	public void addStudent(Student theStudent) {
+		
+		if (students == null) {
+			students = new ArrayList<>();
+		}
+		
+		students.add(theStudent);
+	}
 
 	@Override
 	public String toString() {
